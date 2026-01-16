2bdcc682b9ec3275322c017932db1d5edba2934c
soethiha <usersoe123@gmail.com>
2026-01-16 18:00:30 +0630

    31:59

--- codeItYourSelf/codeItYourSelf.cpp
+++ codeItYourSelf/codeItYourSelf.cpp
@@ -1,20 +1,105 @@
-// codeItYourSelf.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include "olcConsoleGameEngine.h"
+#include <vector>
+
+struct Vec3D {
+	float x = 0.0f;
+	float y = 0.0f;
+	float z = 0.0f;
+};
+
+struct Triangle {
+	Vec3D p[3];
+};
+
+struct Mesh {
+	std::vector<Triangle> tris;
+};
+
+struct Mat4x4 {
+	float m[4][4] = { 0 };
+};
+
+
+class olcEngine3D : public olcConsoleGameEngine {
+public:
+	olcEngine3D() {
+		m_sAppName = L"3D Engine Demo";
+	}
+
+private:
+	Mesh meshCube;
+	Mat4x4 matProj;
+	void MultiplyMatrixVector(Vec3D& i, Vec3D& o, Mat4x4& matrix) {
+		o.x = i.x * matrix.m[0][0] + i.y * matrix.m[1][0] + i.z * matrix.m[2][0] + matrix.m[3][0];
+		o.y = i.x * matrix.m[0][1] + i.y * matrix.m[1][1] + i.z * matrix.m[2][1] + matrix.m[3][1];
+		o.x = i.x * matrix.m[0][2] + i.y * matrix.m[1][2] + i.z * matrix.m[2][2] + matrix.m[3][2];
+		float w = i.x * matrix.m[0][3] + i.y * matrix.m[1][3] + i.z * matrix.m[2][3] + matrix.m[3][3] + matrix.m[3][3];
+
+		if (!w == 0.f) {
+			o.x /= w; o.y /= w; o.z /= w;
+		}
+	}
+public:
+	bool OnUserCreate() override {
+		meshCube.tris = {
+			//South
+			{ 0.0f, 0.0f, 0.0f,		0.0f, 1.0f, 0.0f,	1.0f, 1.0f, 0.0f },
+			{ 0.0f, 0.0f, 0.0f,		1.0f, 1.0f, 0.0f,	1.0f, 0.0f, 0.0f },
+
+			// East
+			{ 1.0f, 0.0f, 0.0f,		1.0f, 1.0f, 0.0f,	1.0f, 1.0f, 1.0f },
+			{ 1.0f, 0.0f, 0.0f,		1.0f, 1.0f, 1.0f,	1.0f, 0.0f, 1.0f },
+
+			// North
+			{ 1.0f, 0.0f, 1.0f,		1.0f, 1.0f, 1.0f,	0.0f, 1.0f, 1.0f },
+			{ 1.0f, 0.0f, 1.0f,		0.0f, 1.0f, 1.0f,	0.0f, 0.0f, 1.0f },
+
+			// West
+			{ 0.0f, 0.0f, 1.0f,		0.0f, 1.0f, 1.0f,	0.0f, 1.0f, 0.0f },
+			{ 0.0f, 0.0f, 1.0f,		0.0f, 1.0f, 0.0f,	0.0f, 0.0f, 0.0f },
+
+			// Top
+			{ 0.0f, 1.0f, 0.0f,		0.0f, 1.0f, 1.0f,	1.0f, 1.0f, 1.0f },
+			{ 0.0f, 1.0f, 0.0f,		1.0f, 1.0f, 1.0f,	1.0f, 1.0f, 0.0f },
+
+			// Bottom
+			{ 1.0f, 0.0f, 1.0f,		0.0f, 0.0f, 1.0f,	0.0f, 0.0f, 0.0f },
+			{ 1.0f, 0.0f, 1.0f,		0.0f, 0.0f, 0.0f,	1.0f, 0.0f, 0.0f },
+
+		};
+
+		//Projection Matrix
+		float fNear = 0.1f;
+		float fFar = 1000.0f;
+		float fFov = 90.0f;
+		float fAspectRatio = (float)ScreenHeight() / (float)ScreenWidth();
+		float fFovRad = 1.0f / tanf(fFov * 0.5f / 180.0f * 3.14159f);
+	
+		matProj.m[0][0] = fAspectRatio * fFovRad;
+		matProj.m[1][1] = fFovRad;
+		matProj.m[2][2] = fFar / (fFar - fNear);
+		matProj.m[3][2] = (-fFar * fNear) / (fFar - fNear);
+		matProj.m[2][3] = 1.0f;
+		matproj.m[3][3] = 0.f;
+		return true;
+	}
+	bool OnUserUpdate(float fElapsedTime) override {
+		Fill(0, 0, ScreenWidth(), ScreenHeight(), PIXEL_SOLID, FG_BLACK);
+
+		// Draw Triangles
+		for (auto& tri : meshCube.tris) {
+
+		}
+		return true;
+	}
+};
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	olcEngine3D demo;
+	if (demo.ConstructConsole(256, 240, 4, 4))
+	{
+		demo.Start();
+	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
